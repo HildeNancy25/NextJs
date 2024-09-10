ab5d74055246d1e8bec18bb7c550b0f31f48a5a8
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-09-10 15:47:43 +0200

    Layouts - header & footer

--- nxt-sample-app/src/app/layout.js
+++ nxt-sample-app/src/app/layout.js
@@ -6,7 +6,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header style={{background:'green', padding:'20px', textAlign:'center'}}>
+          <p>
+            Header
+          </p>
+        </header>
+        {children}
+        <footer style={{background:'green', padding:'20px', textAlign:'center'}}>
+          <p>Footer</p>
+        </footer>
+      </body>
     </html>
   )
 }

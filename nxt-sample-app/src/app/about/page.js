623420c4dd2fb13e5af53c093c3338577a3f37d3
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-09-10 15:16:05 +0200

    routing

---
+++ nxt-sample-app/src/app/about/page.js
@@ -0,0 +1,6 @@
+ export default function About(){
+return <>
+<h1>About page</h1>
+<p>Here is what you need to know about us!</p>
+</>
+}
---
+++ nxt-sample-app/src/app/user/page.js
@@ -0,0 +1,6 @@
+export default function User(){
+    return<>
+    <h1>Manage users</h1>
+    <p>Add, delete, update and supend users in your org.</p>
+    </>
+}
